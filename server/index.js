d8f58a98b495749d9a5d4607fc69018d2fa6a823
mdmunnamia-global <mdmunnamia.global@gmail.com>
2025-03-25 14:05:47 +0600

    Router and controller added

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
-import express from 'express'; //
+import express from 'express'; //1
 import dotenv from 'dotenv'; //2
 import { connectDb } from './database/db.js';
 
 const app = express();
 
-app.get('/',(req,res)=>{
+app.get("/",(req,res)=>{
     res.send('Server is Working');
 })
 
-const port = process.env.PORT;
+
+//importing routes
+import userRoutes from "./routes/user.js"
+//Using routes
+app.use("/api",userRoutes )
+
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
     console.log(`server is running at http://localhost:${port}`)
     connectDb();
 });
-
 
 dotenv.config();
